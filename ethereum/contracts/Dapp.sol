6c6a063312945140d108ff41de1c6803b46b13c3
Jonathan Viray <jfviray7@gmail.com>
2020-08-10 12:45:52 -0400

    Add factory contract architecture to Dapp.sol

--- ethereum/contracts/Dapp.sol
+++ ethereum/contracts/Dapp.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.4.26;
 
 // Dai interface
 contract DaiInterface {
-    function balanceOf(address) public view returns (uint256);
+    function balanceOf(address) public view returns (uint256); // remove
 
     function approve(address, uint256) external returns (bool);
 
     function allowance(address _owner, address _spender)
         public
         view
-        returns (uint256);
+        returns (uint256); // remove
 }
 
 contract CompDaiInterface {
@@ -18,36 +18,97 @@ contract CompDaiInterface {
     function mint(uint256) external returns (uint256);
 }
 
+// Factory Contract
 contract Dapp {
+    address[] public deployedSupplies;
+    mapping(address => uint256) private supplierToSupply;
+
+    // Kovan Dai Contract: 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa
+    // Kovan cDai Contract: 0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD
+
+    function createSupply(
+        address _daiContractAddress,
+        address _compDaiContractAddress
+    ) public returns (address) {
+        // Can't make more than one supply contract!
+        require(
+            supplierToSupply[msg.sender] == 0,
+            "A Supply contract is already registered for this account."
+        );
+
+        address newSupplyAddr = new Supply(
+            _daiContractAddress,
+            _compDaiContractAddress,
+            msg.sender
+        );
+        supplierToSupply[msg.sender] = deployedSupplies.push(newSupplyAddr);
+
+        return newSupplyAddr;
+    }
+
+    function getDeployedSupply(address _userAddr)
+        public
+        view
+        returns (address)
+    {
+        require(
+            supplierToSupply[_userAddr] != 0,
+            "No Supply contract for this account."
+        );
+        uint256 index = supplierToSupply[_userAddr] - 1;
+        return deployedSupplies[index];
+    }
+}
+
+contract Supply {
+    Dapp dapp;
+    address public supplier;
+
     DaiInterface daiContract;
 
     address compDaiContractAddress;
     CompDaiInterface compDaiContract;
 
-    // Kovan Dai Contract: 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa
-    // Kovan cDai Contract: 0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD
+    modifier onlySupplier() {
+        require(msg.sender == supplier);
+        _;
+    }
 
-    constructor(address _daiContractAddress, address _compDaiContractAddress)
-        public
-    {
+    constructor(
+        address _daiContractAddress,
+        address _compDaiContractAddress,
+        address _supplier
+    ) public {
+        dapp = Dapp(msg.sender);
+        supplier = _supplier;
+
         daiContract = DaiInterface(_daiContractAddress);
 
         compDaiContractAddress = _compDaiContractAddress;
         compDaiContract = CompDaiInterface(_compDaiContractAddress);
     }
 
+    // remove
     function getDappDaiBalance() public view returns (uint256) {
         return daiContract.balanceOf(address(this));
     }
 
+    // remove
     function getDappCompDaiBalance() public view returns (uint256) {
         return compDaiContract.balanceOf(address(this));
     }
 
     function supplyDaiToCompound(uint256 _tokenAmount)
         public
+        onlySupplier
         returns (uint256)
     {
+        // Supply contract must have a deposit of Dai greater than or equal to _tokenAmount
+        require(
+            daiContract.balanceOf(address(this)) >= _tokenAmount,
+            "Insufficient amount of Dai deposited to the contract."
+        );
+
         // Approve transfer on the Dai contract
         daiContract.approve(compDaiContractAddress, _tokenAmount);
 
